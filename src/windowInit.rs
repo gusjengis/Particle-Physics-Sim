89936e9cdb2443af9bfecb22a2bd0f6438fb0ac0
Anthony Green <anthony.j.green@outlook.com>
2023-12-19 20:40:45 -0800

    cleaned up windowInit

--- src/windowInit.rs
+++ src/windowInit.rs
@@ -33,25 +33,14 @@ impl Canvas {
                 .expect("Couldn't append canvas to document body.");
         }
 
-        // #[cfg(target_arch = "wasm32")] {
-        //     use winit::dpi::PhysicalSize;
-        //     let w = web_sys::window().unwrap().inner_width().unwrap().as_f64().unwrap();
-        //     let h = web_sys::window().unwrap().inner_height().unwrap().as_f64().unwrap();
-        //     self.window.set_inner_size(PhysicalSize::new(w, h));
-        // }
+        #[cfg(target_arch = "wasm32")] {
+            use winit::dpi::PhysicalSize;
+            let w = web_sys::window().unwrap().inner_width().unwrap().as_f64().unwrap();
+            let h = web_sys::window().unwrap().inner_height().unwrap().as_f64().unwrap();
+            self.window.set_inner_size(PhysicalSize::new(w, h));
+        }
     
         let size = window.inner_size();
-
-        // // https://sotrh.github.io/learn-wgpu/showcase/imgui-demo/ <--  ImGui Tutorial vvv
-        
-        // let mut imgui = imgui::Context::create();
-        // let mut platform = imgui_winit_support::WinitPlatform::init(&mut imgui);
-        // platform.attach_window(
-        //     imgui.io_mut(), 
-        //     &display.window,
-        //     imgui_winit_support::HiDpiMode::Default,
-        // );
-        // imgui.set_ini_filename(None);
 
         Self{
             window,
@@ -61,7 +50,6 @@ impl Canvas {
 
     pub fn updateSize(&mut self, size: PhysicalSize<u32>){
         self.size = size;
-        // self.window.set_inner_size(size);
         #[cfg(target_arch = "wasm32")] {
             use winit::dpi::PhysicalSize;
             let w = web_sys::window().unwrap().inner_width().unwrap().as_f64().unwrap();
@@ -71,24 +59,6 @@ impl Canvas {
     }
 
 }
-// #[cfg_attr(target_arch="wasm32", wasm_bindgen(start))]
-// pub async fn run() {
-//     cfg_if::cfg_if! {
-//         if #[cfg(target_arch = "wasm32")] {
-//             std::panic::set_hook(Box::new(console_error_panic_hook::hook));
-//             console_log::init_with_level(log::Level::Warn).expect("Couldn't initialize logger");
-//         } else {
-//             env_logger::init();
-//         }
-//     }
-    
-    
-    
-
-    // let mut state = State::new(window).await;
-
-
-// }
 
 use winit::window::Window;
 
